649ee1e3f4d78a9b6fecc6b4b7384f49bb6f7939
Vaidas Damosevicius <vd@tib.lt>
2012-05-24 12:53:07 +0300

    New Twitter credentials and few small deployment fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'uN5sOyJNMC6LmzAr4swWXw', 'n1IDzOpdxNAPWAvc76pWLQi0u0lUA2HLxXhoiUs'
+  provider :twitter, 'mRaxR11KHsujUROpi7mdig', 'dRLGaAirZdsbsfOvg8IpHR6pTNNeEDCiA0yrvni8qA'
 end
