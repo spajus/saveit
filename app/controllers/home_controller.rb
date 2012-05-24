e0d7b9b19e046c94d784d3a278f7de11fa403a1f
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-05-24 23:02:19 +0300

    Improved bookmarklet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,14 +8,35 @@ class HomeController < ApplicationController
     if current_user
       @bookmark = current_user.bookmarks.create title: params[:t], url: params[:b]
     else
-      # Javascript assigns window.location to home page, where user can log in.
-      # After login user is redirected back to target url
-      flash[:message] = """Please sign in to use the bookmarklet.
-You will be redirected back to <a href=\"#{params[:b]}\">#{params[:t]}</a>
-afterwards.""".html_safe
-      session[:save_and_return_to] = { :title => params[:t], :url => params[:b]}
+      _session_flash url: params[:b], title: params[:t]
     end
 
     render :layout => false, :content_type => 'text/javascript', :formats => [:js]
   end
+
+  def bookmarklet_failover
+
+    url = params[:b]
+
+    if current_user
+      @bookmark = current_user.bookmarks.create title: url, url: url
+      redirect_to url
+    else
+      _session_flash url: url, title: url
+      redirect_to action: "index"
+    end
+
+  end
+
+  private
+
+  def _session_flash(url, title)
+    # Javascript assigns window.location to home page, where user can log in.
+    # After login user is redirected back to target url
+    flash[:message] = """Please sign in to use the bookmarklet.
+You will be redirected back to <a href=\"#{url}\">#{title}</a>
+afterwards.""".html_safe
+    session[:save_and_return_to] = { title: title, url: url}
+  end
+
 end
