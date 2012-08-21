7c977cb41f03f5db0db69659368ea1ed3d8f09f1
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-08-21 15:24:19 +0300

    RACK -> RAILS

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,5 +1,5 @@
 IMGKit.configure do |config|
-  config.wkhtmltoimage = Rails.root.join('vendor/tools/wkhtmltoimage-0.10.0_rc2', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
+  config.wkhtmltoimage = Rails.root.join('vendor/tools/wkhtmltoimage-0.10.0_rc2', 'wkhtmltoimage-amd64').to_s if ENV['RAILS_ENV'] == 'production'
   config.default_options = {
       :quality => 60
   }
