536e6d225ebd97705bba74c858ca2415f572884f
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2012-10-11 08:06:48 +0300

    Mysql

--- db/migrate/20120818162209_add_description_to_bookmarks.rb
+++ db/migrate/20120818162209_add_description_to_bookmarks.rb
@@ -1,11 +1,11 @@
 class AddDescriptionToBookmarks < ActiveRecord::Migration
   def up
-    #add_column :bookmarks, :description, :string, limit: 2048
+    add_column :bookmarks, :description, :string, limit: 2048
     add_index :bookmarks, :description
   end
 
   def down
     remove_index :bookmarks, :description
-    #remove_column :bookmarks, :description
+    remove_column :bookmarks, :description
   end
 end
